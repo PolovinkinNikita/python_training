9b5634371e542254e2f1fa89e71d48e83711a3a2
Nikita Polovinkin <nickita.polovinckin@gmail.com>
2022-11-08 14:34:03 +0300

    create conftest

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,22 +1,13 @@
 # -*- coding: utf-8 -*-
-import pytest
-from requests import request
 from model.group import Group
-from fixture.application import Application
 
-
-@pytest.fixture
-def app():
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="dsfsdf", header="dfdfd", footer="dsfad"))
+    app.group.create(Group(name="dsfsdf", header="dfdfd", footer="dsfad"))
     app.session.logout()
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
